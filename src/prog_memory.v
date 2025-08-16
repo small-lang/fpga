695bdc099729b39c3da5c6f3d82d1152537c2be6
S1mon <jaforsi.kozak@gmail.com>
2025-08-16 13:07:58 +0200

    fixed program memory output name

--- src/prog_memory.v
+++ src/prog_memory.v
@@ -3,11 +3,11 @@
 module prog_memory
 (
     input       [15:0] addr,
-    output wire [15:0] data_out
+    output wire [15:0] data
 );
     reg [15:0] mem[(1 << 15):0];
 
     initial $readmemb("prog.mem", mem);
-    assign data_out = mem[addr];
+    assign data = mem[addr];
 
 endmodule
