0bf4f11b1954279554409d3132f0fa71f4f3b43b
S1mon <jaforsi.kozak@gmail.com>
2025-08-16 10:47:53 +0200

    fixed program counter
    implemented execute flag
    fixed loading address shifting

--- src/prog_counter.v
+++ src/prog_counter.v
@@ -5,17 +5,19 @@ module prog_counter
     input clk,
     input reset,
     input load_enable,
-    input [15:0] load_data,
+    input [15:0] load_addr,
 
-    output reg [15:0] out
+    output reg [15:0] out,
+    output execute_flag
 );
 
     initial out = 0;
+    assign execute_flag = out[0];
 
     always @ (posedge clk or posedge reset)
     begin
              if (reset)       out <= 0;
-        else if (load_enable) out <= load_data;
+        else if (load_enable) out <= (load_addr << 1);
         else                  out <= out + 1;
     end
 
