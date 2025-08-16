7610e9ea398c4f7142d271d45eb3ac4b81b6a07f
S1mon <jaforsi.kozak@gmail.com>
2025-08-16 13:56:52 +0200

    fixed top level module

--- src/top.v
+++ src/top.v
@@ -1,31 +1,70 @@
-
+  
 module top
 (
-    input  wire       sys_clk,
-    input  wire       reset_raw
-    //output wire [9:0] port_out,
-    //output wire       clk_light
+    input  wire        sys_clk,
+    input  wire        reset_raw,
+    output wire [15:0] port_out,
+    output wire        clk_light
 );
-
     wire reset = !reset_raw;
 
-    wire [15:0] prog_addr;
-    prog_counter pc (
-        .clk (sys_clk),
-        .reset (reset),
-        .load_enable (1'b0),
-        .load_data (),
-        .out (prog_addr)
-    );
 
-    wire [15:0] prog_data;
-    prog_memory pm (
-        .addr (prog_addr),
-        .data_out (prog_data)
+    wire clk;
+    wire ctl_hlt;
+    halt_switch hs(
+        sys_clk,
+        clk,
+        reset,
+        ctl_hlt
     );
 
 
+
+    wire [15:0] prog_mem_addr;
+    wire [15:0] prog_mem_data;
+
+    wire [15:0] arg;
+    wire alu_is_zero;
+
+    //control signals
+    wire ctl_arg;
+    wire ctl_nad;
+    wire ctl_shl;
+    wire ctl_shr;
+    wire ctl_acc;
+    wire ctl_out;
+    wire ctl_read;
+    wire ctl_write;
+
+    control_unit cu (.*);
+
+    prog_memory pm(
+        .addr (prog_mem_addr),
+        .data (prog_mem_data)
+    );
+
+    wire [15:0] bus;
+    cpu cpu(
+        .clk,
+        .arg,
+        .bus,
+        .is_zero (alu_is_zero),
+
+        .ctl_arg,
+        .ctl_nad,
+        .ctl_shl,
+        .ctl_shr,
+        .ctl_read,
+        .ctl_write,
+        .ctl_acc
+    );
+
+
+    register output_reg(
+        .clk,
+        .enable     (ctl_out),
+        .in         (bus),
+        .out        (port_out)
+    );
 
 endmodule
-
-
