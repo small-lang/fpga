218a3487a8bf483605530ab11e305c529d861b82
S1mon <jaforsi.kozak@gmail.com>
2025-08-16 12:15:29 +0200

    implemented data memory

---
+++ src/data_memory.v
@@ -0,0 +1,21 @@
+module data_memory
+(
+    input wire clk,
+    input wire ctl_write_enable,
+
+    input  wire [15:0] addr,
+    input  wire [15:0] data_in,
+    output reg  [15:0] data_out
+);
+
+    reg [15:0] mem[(1 << 15):0];
+
+    always @(posedge clk) begin
+        if (ctl_write_enable)
+            mem[addr] <= data_in;
+        else
+            data_out <= mem[addr];
+    end
+
+
+endmodule
